a0d41f375145aae024688f3b689e56f874ad1b4e
jeffcheasey1337 <90761999+jeffcheasey1337@users.noreply.github.com>
2022-05-12 22:23:05 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <locale>
+
+using namespace std;
+
+double func(double x,double exp= 2.71828)
+{
+	return x*pow(exp,x)-1;
+}
+
+double new_x(double a, double b)
+{
+	return a - (b - a) / (func(b) - func(a)) * func(a);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	double a, b, x0, x1, eps;
+	int it = 1;
+	cout << "Метод хорд для функции y = x*e^(x)-1" << endl;
+	cout << endl;
+	cout << "Введит концы отрезка:" << endl;
+	cin >> a >> b;
+	cout << "Введите точность:" << endl;
+	cin >> eps;
+	do
+	{
+		x0 = new_x(a, b);
+		if (func(b) * func(x0) < 0)
+		{
+			a = x0;
+		}
+		else
+		{
+			b = x0;
+		}
+		x1 = new_x(a, b);
+		it++;
+	} while (abs(x1 - x0) > eps);
+	cout << "Корень уравнения: " << x1 << endl;
+	cout << "Точность: " << eps << endl;
+	cout << "Количество итераций: " << it << endl;
+	
+}
